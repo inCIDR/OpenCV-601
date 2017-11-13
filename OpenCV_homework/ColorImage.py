593d320bc6b2ac2107d69c061b0a137d62bc7c3b
Dennis Your <lavashell@gmail.com>
2017-11-13 14:46:34 -0500

    Fixed Exercise 3

--- OpenCV_homework/ColorImage.py
+++ OpenCV_homework/ColorImage.py
@@ -37,31 +37,3 @@ cv2.imshow("V",V)
 print (imgHSV[25,20])
 
 c = cv2.waitKey(0)
-
-
-
-'''
-vector<Mat> input_planes(3);
-split(src,input_planes);
-Mat channel1_display, channel2_display, channel3_display;
-    imshow("Red",   input_planes[2]);
-    imshow("Green",   input_planes[1]);
-    imshow("Blue",   input_planes[0]);
-
-
-Mat ycrcb_image;
-cvtColor(src, ycrcb_image, CV_BGR2YCrCb);
-split(ycrcb_image,input_planes);
-    imshow("Y",   input_planes[0]);
-    imshow("Cb",   input_planes[1]);
-    imshow("Cr",   input_planes[2]);
-
-
-Mat hsv_image;
-cvtColor(src, hsv_image, CV_BGR2HSV);
-vector<Mat> hsv_planes(3);
-split(hsv_image,hsv_planes);
-    imshow("Hue",   hsv_planes[0]);
-    imshow("Saturation",   hsv_planes[1]);
-    imshow("Value",   hsv_planes[2]);
-'''
--- OpenCV_homework/Noise.py
+++ OpenCV_homework/Noise.py
@@ -1,61 +1,62 @@
 import cv2
+import numpy as np
 
 def Add_salt_pepper_noise(img,pa,pb):
-	RNG rng; // rand number generate
-    int amount1=srcArr.rows*srcArr.cols*pa; //Need to run through all channels
-    int amount2=srcArr.rows*srcArr.cols*pb;
-    for(int counter=0; counter<amount1; ++counter)
-    {
-     srcArr.at<uchar>(rng.uniform( 0,srcArr.rows), rng.uniform(0, srcArr.cols)) =0;
+     sp_noise =img.copy()
+     amount1 = int(img.shape[0]*img.shape[1]*pa)
+     amount2 = int(img.shape[0]*img.shape[1]*pb)
+     
+     for i in range(0,amount1):
+     	sp_noise[int(np.random.uniform(0,img.shape[0])),int(np.random.uniform(0,img.shape[1]))] = 0
+     for i in range(0,amount2):
+     	sp_noise[int(np.random.uniform(0,img.shape[0])),int(np.random.uniform(0,img.shape[1]))] = 255
 
-    }
-     for (int counter=0; counter<amount2; ++counter)
-     {
-     srcArr.at<uchar>(rng.uniform(0,srcArr.rows), rng.uniform(0,srcArr.cols)) = 255;
-     }
+     return sp_noise
 
 def Add_gaussian_noise(img,mean,sigma):
-    Mat NoiseArr = srcArr.clone();
-    RNG rng;
-    rng.fill(NoiseArr, RNG::NORMAL, mean,sigma);  
-    add(srcArr, NoiseArr, srcArr);  
+	NoiseArr = img.copy()
+	cv2.randn(NoiseArr,mean,sigma)
+	NoiseArr = cv2.add(img,NoiseArr)
+	return NoiseArr
 
 image = cv2.imread("Test_images/baboon.jpg", 0);
 cv2.namedWindow( "Original image", cv2.WINDOW_NORMAL );
 cv2.imshow( "Original image", image);
 
-noise_img = image.clone();
+noise_img = image.copy();
 mean = 0;
-sigma = 50;
-Add_gaussian_noise(noise_img,mean,sigma);
+sigma = 60;
+noise_img = Add_gaussian_noise(noise_img,mean,sigma);
 cv2.imshow( "Gaussian Noise", noise_img);
 
-noise_dst = noise_img.clone();
-cv2.blur(noise_dst,noise_dst,cv2.Size(3,3));
+noise_dst = noise_img.copy();
+noise_dst = cv2.blur(noise_dst,(7,7));
 cv2.imshow( "Box filter", noise_dst);
 
-noise_dst1 = noise_img.clone()
-cv2.GaussianBlur(noise_dst1, noise_dst1, cv2.Size(3,3),1.5);
+noise_dst1 = noise_img.copy()
+noise_dst1 = cv2.GaussianBlur(noise_dst1, (7,7),1.5);
 cv2.imshow( "Gaussian filter", noise_dst1);
 
-noise_dst2 = noise_img.clone();
-cv2.medianBlur(noise_dst2,noise_dst2,3);
+noise_dst2 = noise_img.copy();
+noise_dst2 = cv2.medianBlur(noise_dst2,7);
 cv2.imshow( "Median filter", noise_dst2);
 
-noise_img2 = image.clone()
-pa = 0.01;
-pb = 0.01;
-Add_salt_pepper_noise(noise_img2,pa,pb)
+noise_img2 = image.copy()
+pa = 0.1;
+pb = 0.1;
+noise_img2 = Add_salt_pepper_noise(noise_img2,pa,pb)
 cv2.imshow( "Salt and Pepper Noise", noise_img2);
 
-noise_dst3 = noise_img2.clone();
-cv2.blur(noise_dst3,noise_dst3,cv2.Size(3,3));
-cv2.imshow( "Box filter", noise_dst3);
+noise_dst3 = noise_img2.copy();
+noise_dst3 = cv2.blur(noise_dst3,(7,7));
+cv2.imshow( "Box filter SP", noise_dst3);
 
-noise_dst4 = noise_img2.clone();
-cv2.GaussianBlur(noise_dst4,noise_dst4,cv2.Size(3,3),1.5);
-cv2.imshow( "Gaussian filter", noise_dst4);
+noise_dst4 = noise_img2.copy();
+noise_dst4 = cv2.GaussianBlur(noise_dst4,(7,7),1.5);
+cv2.imshow( "Gaussian filter SP", noise_dst4);
 
-noise_dst5 = noise_img2.clone();
-cv2.medianBlur(noise_dst5,noise_dst5,3);
-cv2.imshow( "Median filter", noise_dst5);
+noise_dst5 = noise_img2.copy();
+noise_dst5 = cv2.medianBlur(noise_dst5,7);
+cv2.imshow( "Median filter SP", noise_dst5);
+
+c = cv2.waitKey(0)
